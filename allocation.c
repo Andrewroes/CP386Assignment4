4dc7f7bdf018e9d87f444df0c9f14518e133fe10
Andrewroes <roes1050@mylaurier.ca>
2022-07-22 15:53:19 -0400

    small start

--- allocation.c
+++ allocation.c
@@ -5,4 +5,16 @@
 #include <pthread.h>
 #include <ctype.h>
 
+#define MAX = 1000000 - 1;
+
+int main() {
+    char input[20];
+    printf(MAX);
+    while(strcmp(input,"Exit")) {
+        printf("Enter your option (1, 2, 3, or 'Exit'): ");
+        scanf("%s", input);
+
+    }
+    return 0;
+}
 
