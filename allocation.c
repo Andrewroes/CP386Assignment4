3e58b2dad29614f91e007e7d9b5fd2318dbf0a8e
joeyus <joeyus.w@gmail.com>
2022-07-22 17:57:43 -0400

    added structures

--- allocation.c
+++ allocation.c
@@ -1,14 +1,29 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <pthread.h>
 #include <ctype.h>
 
-int MAX;
+#define MAX = 1000000 - 1;
+int alloc_mem;
+struct open_node{
+    int size;
+    int start;
+    int end;
+    struct open_node *next;
+} open;
+struct memory_node{
+    int size;
+    int start;
+    int end;
+    char memory;
+    struct memory_node *next;
+    
+} closed;
+
+void closed_mem(open **free_memory,closed **allocated_memory,char alg, char*procname, int size );
+void open_mem(open **free_memory, closed **alloc_memory, char *procname);
 
 int main() {
     char input[20];
+    printf(MAX);
     while(strcmp(input,"Exit")) {
         printf("Enter your option (1, 2, 3, or 'Exit'): ");
         scanf("%s", input);
@@ -16,4 +31,3 @@ int main() {
     }
     return 0;
 }
-
